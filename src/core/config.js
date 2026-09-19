8b7592fa10f0fbc4e43c7467f29eb82a8c64fb9d
ZYON-FILMVERSO <zyon7ago@gmail.com>
2026-09-19 15:48:30 -0500

    Add configuration file for bot settings

---
+++ src/core/config.js
@@ -0,0 +1,8 @@
+export default {
+  BOT_NAME: "ZYON-FVS™",
+  PREFIX: "",
+  TIMEZONE: "America/Lima",
+  HORARIO: { inicio: 6, fin: 24 }, // 6am a 12am
+  COMANDOS_USUARIOS: ["hoy", "dia", "efemerides", "efemérides"],
+  COMANDOS_ADMIN: ["efemerides on", "efemerides off"]
+}
