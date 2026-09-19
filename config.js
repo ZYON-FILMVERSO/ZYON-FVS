9ddcabaa071165051d6434bcf6b6e653190054cb
ZYON-FILMVERSO <zyon7ago@gmail.com>
2026-09-18 22:31:52 -0500

    Add configuration constants for bot settings

---
+++ config.js
@@ -0,0 +1,4 @@
+export const OWNER = ["51976379730@s.whatsapp.net"] // tu número con @s.whatsapp.net
+export const PREFIX = "!"
+export const BOT_NAME = "ZYON-FVS™"
+export const MONGODB_URL = process.env.MONGODB_URL || ""
